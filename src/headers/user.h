57942d6d2e9b9da1a4e1e5aafef91efe766a72f5
RodrigoLiC <rodrigo.li@utec.edu.pe>
2024-07-15 23:18:03 -0500

    Push Final

--- src/headers/user.h
+++ src/headers/user.h
@@ -1,4 +1,4 @@
-//src/headers/user.h
+// src/headers/user.h
 #ifndef PROYECTO_PROGRAMACION3_USER_H
 #define PROYECTO_PROGRAMACION3_USER_H
 
@@ -11,6 +11,15 @@ class User {
 private:
     std::vector<int> likedMovies;
     std::vector<int> watchLaterMovies;
+
+    bool isLikedMovie(int movieIndex) const {
+        for (int index : likedMovies) {
+            if (index == movieIndex) {
+                return true;
+            }
+        }
+        return false;
+    }
 
 public:
     void likeMovie(int movieIndex) {
@@ -26,16 +35,51 @@ public:
     void showLikedMovies(Database* db) const {
         std::cout << "Liked Movies:\n";
         for (int index : likedMovies) {
-            MovieFactory::displayMovie(MovieFactory::FULL,db->getMovies()[index]);
+            MovieFactory::displayMovie(MovieFactory::FULL, db->getMovies()[index]);
         }
     }
 
     void showWatchLaterMovies(Database* db) const {
         std::cout << "Watch Later Movies:\n";
         for (int index : watchLaterMovies) {
-            MovieFactory::displayMovie(MovieFactory::FULL,db->getMovies()[index]);
+            MovieFactory::displayMovie(MovieFactory::FULL, db->getMovies()[index]);
         }
+    }
+
+    void showRecommendations(Database* db) const {
+        std::unordered_set<std::string> likedTags;
+        std::vector<Movie> movies = db->getMovies();
+        std::cout << "Collecting tags from liked movies...\n";
+        for (int index: likedMovies) {
+            std::vector<std::string> tags = db->getMovies()[index].tags;
+            for (auto tag: tags) {
+                likedTags.insert(tag);
+            }
+        }
+
+        std::cout << "Finding movies with similar tags...\n";
+        std::vector<int> recommendedMovies;
+        for (int i = 0; i < movies.size(); ++i) {
+            if (isLikedMovie(i)) {
+                continue;
+            }
+            for (auto tag: movies[i].tags) {
+                if (likedTags.find(tag) != likedTags.end()) {
+                    recommendedMovies.push_back(i);
+                    break;
+                }
+            }
+            if(recommendedMovies.size() > 5) {
+                break;
+            }
+        }
+
+        std::cout << "Here are some movies you may like:\n";
+        for (int i = 0; i < recommendedMovies.size(); ++i) {
+            MovieFactory::displayMovie(MovieFactory::PREVIEW, movies[recommendedMovies[i]]);
+        }
+        std::cout << "Going back to menu...\n";
     }
 };
 
-#endif
+#endif // PROYECTO_PROGRAMACION3_USER_H
